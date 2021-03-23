2392e07b354358df382c7e7fa1483af7d2558a65
pnda <43609023+ThePNDA@users.noreply.github.com>
2021-03-23 18:17:56 +0100

    Rename application interanlly to 'bananas'

--- bin/bananas_cli.dart
+++ bin/bananas_cli.dart
@@ -7,7 +7,7 @@ import 'commands/packages_command.dart';
 import 'commands/login_command.dart';
 
 void main(List<String> args) async {
-  var runner = CommandRunner('bananas_cli', 'A CLI for uploading and managing BaNaNaS packages.')
+  var runner = CommandRunner('bananas', 'A CLI for uploading and managing BaNaNaS packages.')
     ..addCommand(ManageCommand())
     ..addCommand(LoginCommand())
     ..addCommand(PackagesCommand());
